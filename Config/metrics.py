a70b2ea3df6362774c46f15762c296f74956eaae
P-ZYang <113532877+P-ZYang@users.noreply.github.com>
2026-04-27 15:21:34 +0800

    Update metrics.py

--- Config/metrics.py
+++ Config/metrics.py
@@ -1,7 +1,32 @@
+import threading
+
 import numpy as np
 import torch
 from skimage import measure
 
+def cal_tp_pos_fp_neg(output, target, nclass, score_thresh):
+    predict = (output > score_thresh).float()
+    if len(target.shape) == 3:
+        target = target.unsqueeze(dim=0)
+        target.to('cuda', torch.float)
+
+    elif len(target.shape) == 4:
+        target = target.float()
+    else:
+        raise ValueError("Unknown target dimension")
+    # 现在predict中高于阈值的部分为全1矩阵   target是GT
+
+    intersection = predict * ((predict == target).float())
+
+    tp = intersection.sum()  # 对的预测为对的
+    fp = (predict * ((predict != target).float())).sum()  # 错的预测为对的 虚警像素数
+    tn = ((1 - predict) * ((predict == target).float())).sum()  # 错的预测为错的
+    fn = (((predict != target).float()) * (1 - predict)).sum()  # 对的预测为错的
+    pos = tp + fn  # 标签中 阳性的个数
+    neg = fp + tn  # 标签中 阴性的个数
+    class_pos = tp + fp  # 检测出的个数
+
+    return tp, pos, fp, neg, class_pos
 
 class ROCMetric():
     """Computes pixAcc and mIoU metric scores
@@ -24,12 +49,12 @@ class ROCMetric():
     def update(self, preds, labels):
         for iBin in range(self.bins + 1):
             # score_thresh = (iBin + 0.0) / self.bins
-            score_thresh = -30 + iBin * (255 / self.bins)
+            score_thresh = (0.0 + iBin) / self.bins
             # print(iBin, "-th, score_thresh: ", score_thresh)
             i_tp, i_pos, i_fp, i_neg, i_class_pos = cal_tp_pos_fp_neg(preds, labels, self.nclass, score_thresh)
             self.tp_arr[iBin] += i_tp
             self.pos_arr[iBin] += i_pos
-            self.fp_arr[iBin] += i_fp
+            self.fp_arr[iBin] += i_fp  # 虚警像素数
             self.neg_arr[iBin] += i_neg
             self.class_pos[iBin] += i_class_pos
 
@@ -39,8 +64,9 @@ class ROCMetric():
         FP = self.fp_arr / (self.neg_arr + self.pos_arr)
         recall = self.tp_arr / (self.pos_arr + 0.001)  # recall = TP/(TP+FN)
         precision = self.tp_arr / (self.class_pos + 0.001)  # precision = TP/(TP+FP)
+        f1_score = (2.0 * recall[5] * precision[5]) / (recall[5] + precision[5] + 0.00001)
 
-        return tp_rates, fp_rates, recall, precision, FP
+        return tp_rates, fp_rates, recall, precision, FP, f1_score
 
     def reset(self):
         self.tp_arr = np.zeros([11])
@@ -49,20 +75,18 @@ class ROCMetric():
         self.neg_arr = np.zeros([11])
         self.class_pos = np.zeros([11])
 
-
 class mIoU():
-
     def __init__(self):
         super(mIoU, self).__init__()
         self.reset()
 
     def update(self, preds, labels):
-        correct, labeled = batch_pix_accuracy(preds, labels)
+        correct, labeled = batch_pix_accuracy(preds, labels)  # labeled: GT中目标的像素数目   correct:预测正确的像素数
         inter, union = batch_intersection_union(preds, labels)
-        self.total_correct += correct  # 预测正确的像素数
-        self.total_label += labeled  # GT目标的像素数
-        self.total_inter += inter  # 交集
-        self.total_union += union  # 并集
+        self.total_correct += correct
+        self.total_label += labeled
+        self.total_inter += inter
+        self.total_union += union
 
     def get(self):
         pixAcc = 1.0 * self.total_correct / (np.spacing(1) + self.total_label)
@@ -143,9 +167,9 @@ def batch_pix_accuracy(output, target):
         raise ValueError("Unknown target dimension")
 
     assert output.shape == target.shape, "Predict and Label Shape Don't Match"
-    predict = (output > 0).float()  # 将output 从 True Flase 转成 1 0
-    pixel_labeled = (target > 0).float().sum()  # GF中 1的个数
-    pixel_correct = (((predict == target).float()) * ((target > 0)).float()).sum()  # 预测对的个数
+    predict = (output > 0).float()
+    pixel_labeled = (target > 0).float().sum()
+    pixel_correct = (((predict == target).float()) * ((target > 0)).float()).sum()
     assert pixel_correct <= pixel_labeled, "Correct area should be smaller than Labeled"
     return pixel_correct, pixel_labeled
 
@@ -171,3 +195,106 @@ def batch_intersection_union(output, target):
     assert (area_inter <= area_union).all(), \
         "Error: Intersection area should be smaller than Union area"
     return area_inter, area_union
+
+def batch_intersection_union_n(output, target, nclass, score_thresh):
+    """nIoU"""
+    mini = 1
+    maxi = 1  # nclass
+    nbins = 1  # nclass
+    outputnp = output.detach().cpu().numpy()
+    # outputsig = F.sigmoid(output).detach().cpu().numpy()
+    # outputsig = nd.sigmoid(output).asnumpy()
+    predict = (outputnp > 0.5).astype('int64')
+    # predict = predict.detach().cpu().numpy()
+    # predict = (output.asnumpy() > 0).astype('int64') # P
+    if len(target.shape) == 3:
+        target = np.expand_dims(target, axis=1).asnumpy().astype('int64')  # T
+    elif len(target.shape) == 4:
+        target = target.cpu().numpy().astype('int64')  # T
+    else:
+        raise ValueError("Unknown target dimension")
+    intersection = predict * (predict == target)  # TP  交集
+
+    num_sample = intersection.shape[0]
+    area_inter_arr = np.zeros(num_sample)
+    area_pred_arr = np.zeros(num_sample)
+    area_lab_arr = np.zeros(num_sample)
+    area_union_arr = np.zeros(num_sample)
+    for b in range(num_sample):
+        # areas of intersection and union
+        area_inter, _ = np.histogram(intersection[b], bins=nbins, range=(mini, maxi))
+        area_inter_arr[b] = area_inter
+
+        area_pred, _ = np.histogram(predict[b], bins=nbins, range=(mini, maxi))
+        area_pred_arr[b] = area_pred
+
+        area_lab, _ = np.histogram(target[b], bins=nbins, range=(mini, maxi))
+        area_lab_arr[b] = area_lab
+
+        area_union = area_pred + area_lab - area_inter
+        area_union_arr[b] = area_union
+
+        assert (area_inter <= area_union).all(), \
+            "Intersection area should be smaller than Union area"
+
+    return area_inter_arr, area_union_arr
+
+class SamplewiseSigmoidMetric(object):
+    """Computes pixAcc and mIoU metric scores
+    """
+
+    def __init__(self, nclass, score_thresh=0.5):
+        self.nclass = nclass
+        self.score_thresh = score_thresh
+        self.lock = threading.Lock()
+        self.reset()
+
+    def update(self, preds, labels):
+        """Updates the internal evaluation result.
+
+        Parameters
+        ----------
+        labels : 'NDArray' or list of `NDArray`
+            The labels of the data.
+
+        preds : 'NDArray' or list of `NDArray`
+            Predicted values.
+        """
+
+        def evaluate_worker(self, label, pred):
+            inter_arr, union_arr = batch_intersection_union_n(
+                pred, label, self.nclass, self.score_thresh)
+            with self.lock:
+                self.total_inter = np.append(self.total_inter, inter_arr)
+                self.total_union = np.append(self.total_union, union_arr)
+
+        if isinstance(preds, torch.Tensor):
+            evaluate_worker(self, labels, preds)
+        elif isinstance(preds, (list, tuple)):
+            threads = [threading.Thread(target=evaluate_worker,
+                                        args=(self, label, pred),
+                                        )
+                       for (label, pred) in zip(labels, preds)]
+            for thread in threads:
+                thread.start()
+            for thread in threads:
+                thread.join()
+
+    def get(self):
+        """Gets the current evaluation result.
+
+        Returns
+        -------
+        metrics : tuple of float
+            pixAcc and mIoU
+        """
+        IoU = 1.0 * self.total_inter / (np.spacing(1) + self.total_union)
+        nIoU = IoU.mean()
+        return nIoU
+
+    def reset(self):
+        """Resets the internal evaluation result to initial state."""
+        self.total_inter = np.array([])
+        self.total_union = np.array([])
+        self.total_correct = np.array([])
+        self.total_label = np.array([])
